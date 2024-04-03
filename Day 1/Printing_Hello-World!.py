84cf5c204f77ffdafe37ff619dac87ce64acef66
jai <jackie.chauhan89@gmail.com>
2024-04-03 20:24:31 +0530

    More printing tyeps added

--- Day 1/Printing_Hello-World!.py
+++ Day 1/Printing_Hello-World!.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+#print("Hello World!")
+
+
+#when something print in double qoute so use entire line in single qoute
+#print('This is an "Example" of a string')
+
+#when i want to somthing ignore in my code use \ (backslash)
+#example
+#print("Please \"ignore\" this you can continue.")
+
+#how to print somthing from new line with using \n , use it wihtout any gap
+#print("Hello sir,\nI am a student.\nPlease pay attention.")
+
+#how to concatinate a string with another string.
+#print("Hello"+" "+"\nJai")
+
