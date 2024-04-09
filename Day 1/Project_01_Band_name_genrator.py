f0e6b874a1e377861138c9daa6a92b35cfe4d9bd
jai <jackie.chauhan89@gmail.com>
2024-04-09 22:41:59 +0530

    Project_added

---
+++ Day 1/Project_01_Band_name_genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name genrator.")
+city_name = input("Please type Your City Name.")
+pet_name = input("Please type Your Pet Name.")
+print(f"Your Band Name is {pet_name +" "+ city_name}")
